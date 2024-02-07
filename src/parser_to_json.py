804d54a87d89856b8909612a9f21f91a4b4d38b0
AlexPolarBear <durn_alex@mail.ru>
2024-02-07 22:44:54 +0300

    Add dictionary for json

--- src/parser_to_json.py
+++ src/parser_to_json.py
@@ -3,46 +3,83 @@ import glob
 import os
 import sys
 import logging
+import json
+# from dataclasses import dataclass, asdict
 
-logger = logging.getLogger('logger')
- 
-path_conf = sys.argv[1]
-numb_conf = sys.argv[2]
 
-# argv = sys.argv[1:]
-# try:
-#     options, args = getopt.getopt(argv, "p:n:",
-#                                   ["path_conf = """, 
-#                                    "numb_conf = """])
-# except:
-#     print("Error Message ")
- 
-# for name, value in options:
-#     if name in ['-p', '--path']:
-#         path_conf = value
-#     elif name in ['-n', '--numb']:
-#         numb_conf = value
- 
-# print(path_conf + " " + numb_conf)
+# @dataclass
+# class InventoryItem:
+#     configFile: str
+#     configurationID: float
+#     configuratioData: int = 0
 
-# print(path_conf)
-# print(numb_conf)
-try:
-    with open(path_conf, 'r') as f:
-        file = f.read().split("\n")
+
+
+class Parser():
+    """
+    A class used to parse configuration file 
+    and save data to json file.
+    """
+
+    def __init__(self, path_conf, numb_conf = 1, data = {}):
+        self.path_conf = path_conf
+        self.numb_conf = numb_conf
+        self.data = data
+
+        self.data["configurationFile"] = self.path_conf
+        self.data["configurationID"] = self.numb_conf
+
+    def find_config(self) -> (tuple[str, list[str]] | None):
+        """
+        Returns mode and path to file(s) or None
+        if there's an error occured.
+        """
+        
         try:
-            index = file.index(f"#{numb_conf}")
-            mode = file[index+1].replace("#mode: ", "")
-            path = file[index+2].replace("#path: ", "").split(", ")
-        except ValueError as err:
-            print(f"There are only {round(len(file)/4)} positions in the config.")
-            logger.error(err.args[0])
-except OSError as err:
-    print("Make sure that the request is correct.")
-    logger.error(err.args[1])
+            with open(self.path_conf, 'r') as f:
+                file = f.read().split("\n")
+                
+                try:
+                    index = file.index(f"#{self.numb_conf}")
+                    mode = file[index+1].replace("#mode: ", "")
+                    path = file[index+2].replace("#path: ", "").split(", ")
+
+                    new_data = {"mode": mode, "path": path}
+                    self.data["configuratioData"] = new_data
+
+                    return mode, path
+                
+                except ValueError as err:
+                    print(f"There are only {round(len(file)/4)} positions in the config.")
+                    return logger.error(err.args[0])
+        
+        except OSError as err:
+            print("Make sure that the request is correct.")
+            return logger.error(err.args[1])
+
+    def parse_file(self):
+        print(json.dumps(self.data))
+
+    def save_json(self):
+        print("implement me")
 
 # for filename in glob.glob(os.path.join(path_conf, '*.*')):
 #    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
 #       # do your stuff
 #       print(filename)
 #       print(f.read())
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger('logger')
+
+    path = sys.argv[1]
+    numb = int(sys.argv[2])
+
+    parser = Parser(path, numb)
+
+    try:
+        mode, path = parser.find_config()
+        parser.parse_file()
+    except TypeError as err:
+        print(err)
