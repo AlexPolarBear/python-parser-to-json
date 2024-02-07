c957e6742aa64834b250a422befe59415c700c1b
AlexPolarBear <durn_alex@mail.ru>
2024-02-08 00:45:36 +0300

    Update examples, add method in parser

--- src/parser_to_json.py
+++ src/parser_to_json.py
@@ -4,15 +4,7 @@ import os
 import sys
 import logging
 import json
-# from dataclasses import dataclass, asdict
-
-
-# @dataclass
-# class InventoryItem:
-#     configFile: str
-#     configurationID: float
-#     configuratioData: int = 0
-
+import datetime
 
 
 class Parser():
@@ -21,53 +13,69 @@ class Parser():
     and save data to json file.
     """
 
-    def __init__(self, path_conf, numb_conf = 1, data = {}):
+    def __init__(self, path_conf, numb_conf = 1):
         self.path_conf = path_conf
         self.numb_conf = numb_conf
-        self.data = data
+
+        self.data = {}
+        self.mode = ""
+        self.path = []
 
         self.data["configurationFile"] = self.path_conf
         self.data["configurationID"] = self.numb_conf
 
-    def find_config(self) -> (tuple[str, list[str]] | None):
+    def find_config(self) -> bool:
         """
-        Returns mode and path to file(s) or None
-        if there's an error occured.
+        Returns True if mode and path to file(s) find successfully
+        or False if there's an error occured.
         """
         
         try:
-            with open(self.path_conf, 'r') as f:
+            with open(self.path_conf, "r") as f:
                 file = f.read().split("\n")
-                
+
                 try:
                     index = file.index(f"#{self.numb_conf}")
-                    mode = file[index+1].replace("#mode: ", "")
-                    path = file[index+2].replace("#path: ", "").split(", ")
+                    self.mode = file[index+1].replace("#mode: ", "")
+                    paths = file[index+2].replace("#path: ", "")
+                    self.path = paths.split(", ")
 
-                    new_data = {"mode": mode, "path": path}
+                    new_data = {"mode": self.mode, "path": paths}
                     self.data["configuratioData"] = new_data
 
-                    return mode, path
+                    return True
                 
                 except ValueError as err:
-                    print(f"There are only {round(len(file)/4)} positions in the config.")
-                    return logger.error(err.args[0])
+                    logger.error(f"There are only {round(len(file)/4)} positions in the config.\n"
+                                  + err.args[0])
+                    return False
         
         except OSError as err:
-            print("Make sure that the request is correct.")
-            return logger.error(err.args[1])
+            logger.error("Make sure that the request is correct.\n" + err.args[1])
+            return False
 
-    def parse_file(self):
-        print(json.dumps(self.data))
+    def parse_file(self) -> None:
+        i = 1
+        outuput_data = {}
+        for file in self.path:
+            with open(file, "r") as f:
+                inside = f.read().split("\n")
+                inside_data = {}
+                ii = 1
+                for one in inside:
+                    inside_data[ii] = one
+                    ii += 1
+            outuput_data[i] = inside_data
+            i += 1
+        print(outuput_data)
+        # print(json.dumps(self.data))
+        self.data["out"] = outuput_data
+        self.save_json()
 
     def save_json(self):
-        print("implement me")
-
-# for filename in glob.glob(os.path.join(path_conf, '*.*')):
-#    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
-#       # do your stuff
-#       print(filename)
-#       print(f.read())
+        date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
+        with open(f"../data{date}.json", "w", encoding="utf-8") as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=3)
 
 
 if __name__ == "__main__":
@@ -77,9 +85,6 @@ if __name__ == "__main__":
     numb = int(sys.argv[2])
 
     parser = Parser(path, numb)
-
-    try:
-        mode, path = parser.find_config()
+    ok = parser.find_config()
+    if ok:
         parser.parse_file()
-    except TypeError as err:
-        print(err)
